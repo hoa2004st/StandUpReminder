d472cf9859d923f218946b21b1818c1ec88e5d2e
unknown <hoa2004st.com>
2024-06-05 09:23:57 +0700

    ...

--- StandUpReminder/src/Record.java
+++ StandUpReminder/src/Record.java
@@ -1,31 +1,68 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.Duration;
+import java.util.List;
+import java.util.Scanner;
 
 public class Record {
     LocalDate date;
     LocalTime startTime;
     LocalTime endTime;
     Duration workTime;
+    Duration totalTime;
+    LocalTime temp;
+    float ratio;
 
-    int No;
-    Record(){    }
+    Record(){}
 
-    public void start(){
-        this.startTime = LocalTime.now();
-        this.date = LocalDate.now();
+    public LocalDate getDate() {
+        return date;
     }
-    public void work(){}
 
-    public void rest() {}
+    public Duration getWorkTime() {
+        return workTime;
+    }
 
-    public void pause(){}
+    public Duration getTotalTime() {
+        return workTime;
+    }
 
+    public float getRatio() {
+        return ratio;
+    }
 
+    public void work(){
+        if (this.startTime == null) {
+            this.startTime = LocalTime.now();
+            this.date = LocalDate.now();
+        }
+        this.temp = LocalTime.now();
+    }
 
-    public Duration endSession(){
+    public void rest() {
+        this.workTime = this.workTime.plus(Duration.between(this.temp, LocalTime.now()));
+    }
+
+    public void endSession(){
         this.endTime = LocalTime.now();
-        return Duration.between(this.endTime, this.startTime);
+        this.totalTime = Duration.between(this.endTime, this.startTime);
+        this.ratio = workTime.dividedBy(totalTime);
+        if (totalTime.compareTo(Duration.parse("3S")) > 0){
+            saveRecord(this);
+        }
     }
+
+    public void saveRecord (Record record) {
+        String filePath = "records.csv";
+        try (PrintWriter writer = new PrintWriter((new File(filePath)))){
+            writer.println(record.getDate() + "," + getTotalTime() + "," + getWorkTime() + "," + getRatio());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 }
---
+++ StandUpReminder/src/RecordLoader.java
@@ -0,0 +1,33 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class RecordLoader {
+    String date;
+    String totalTime;
+    String workTime;
+    String ratio;
+    public static List<RecordLoader> records;
+
+    RecordLoader(String date, String totalTime, String workTime, String ratio) {
+        this.date = date;
+        this.totalTime = totalTime;
+        this.workTime = workTime;
+        this.ratio = ratio;
+    }
+
+    public static List<RecordLoader> loadRecords(String pathToCsvFile) {
+        String filePath = "records.csv";
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            while (scanner.hasNextLine()) {
+                String[] data = scanner.nextLine().split(",");
+                RecordLoader recordLoader = new RecordLoader(data[0], data[1], data[2], data[3]);
+                records.add(recordLoader);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        };
+        return RecordLoader.records;
+    }
+}
